4fbd65b9332e0f04a490cce6219b05d49d16c2ea
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-02 01:04:35 +0330

    [Melobit Api] add getLatestSliders & getLatestSong

--- src/api/MelobitApi.js
+++ src/api/MelobitApi.js
@@ -1,1 +1,9 @@
-import { getAxiosInstance } from "./httpClient";
+import { getAxiosInstance } from "./HttpClient";
+
+const getLatestSliders = () => getAxiosInstance.get('/song/slider/latest');
+const getLatestSong = () => getAxiosInstance.get("/song/new/0/11");
+
+export {
+    getLatestSliders,
+    getLatestSong
+};
