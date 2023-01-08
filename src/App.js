ce58c6f31df6cbd034d9fb6e5bcbc6ab064127ed
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-08 15:05:13 +0330

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
 
 function App() {
-
-    return (
-
-        <Layout>
-           <Home />
-        </Layout>
-
-    );
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="seach" element={<Search />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
