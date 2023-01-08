f884dc00cbf112323bc58cc60252f2a774ad9e55
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-08 15:05:42 +0330

    search page component

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeIcon from '../../assets/icon/home.svg';
+import Header from '../../components/Header/Header';
+
+export default function Search() {
+  return (
+    <>
+        <Header logo={HomeIcon} title={"Search"} />
+    </>)
+}
