066a62a64b5c0441b0b4035417305c799f51a625
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-08 15:05:07 +0330

    add api melobit

--- src/api/MelobitApi.js
+++ src/api/MelobitApi.js
@@ -1,9 +1,19 @@
 import { getAxiosInstance } from "./HttpClient";
 
 const getLatestSliders = () => getAxiosInstance.get('/song/slider/latest');
-const getLatestSong = () => getAxiosInstance.get("/song/new/0/11");
+const getLatestSong = (start,end) => getAxiosInstance.get(`/song/new/${start}/${end}`);
+const getTopDaySong = (start,end) => getAxiosInstance.get(`/song/top/day/${start}/${end}`);
+const getTopWeekSong = (start,end) => getAxiosInstance.get(`/song/top/week/${start}/${end}`);
+const getTrendingArtist = (start,end) => getAxiosInstance.get(`/artist/trending/${start}/${end}`);
+const getSongDetails = (songId) => getAxiosInstance.get(`/song/${songId}`);
+const getSearchArtistOrSong = (query) => getAxiosInstance.get(`/search/query/${query}/0/50`);
 
 export {
     getLatestSliders,
-    getLatestSong
+    getLatestSong,
+    getTopDaySong,
+    getTopWeekSong,
+    getTrendingArtist,
+    getSongDetails,
+    getSearchArtistOrSong
 };
