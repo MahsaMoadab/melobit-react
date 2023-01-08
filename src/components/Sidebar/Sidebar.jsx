9acb4f04262283a177f45d106546d0986ac8e990
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-08 15:06:42 +0330

    add link page

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import Logo from '../../assets/icon/logo.svg';
 import HomeIcon from '../../assets/icon/home.svg';
 import SearchIcon from '../../assets/icon/search.svg';
 import HistoryIcon from '../../assets/icon/history.svg';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
@@ -13,10 +14,15 @@ export default function Sidebar() {
 
             <div className="sidebar_menu">
                 <div className="sidebar_menu_item">
-                    <img src={HomeIcon} alt="" />
+                    <Link to={'/'}>
+                         <img src={HomeIcon} alt="" />
+                    </Link>
+                   
                 </div>
                 <div className="sidebar_menu_item">
+                <Link to={'search'}>
                     <img src={SearchIcon} alt="" />
+                    </Link>
                 </div>
                 <div className="sidebar_menu_item">
                     <img src={HistoryIcon} alt="" />
