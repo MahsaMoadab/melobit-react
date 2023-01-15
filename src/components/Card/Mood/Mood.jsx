8fdb92c6dac742028e7f0adf61dd2ae24097104a
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-15 22:58:56 +0330

    change tooltip mood

--- src/components/Card/Mood/Mood.jsx
+++ src/components/Card/Mood/Mood.jsx
@@ -10,41 +10,33 @@ export default function Mood() {
     return (
         <div className='mt-2'>
 
-
-            <h5>
-                What is your Mood?
-            </h5>
+            <OverlayTrigger placement="top-start" overlay={tooltip}>
+                <h5>
+                    What is your Mood?
+                </h5></OverlayTrigger>
             <Row className='mood'>
                 <Col xxl={3} xl={3} lg={3} md={3}>
-                    <OverlayTrigger placement="auto" overlay={tooltip}>
-                        <div className="mood_item mood_work" >
-                            Work
-                        </div>
-                    </OverlayTrigger>
+                    <div className="mood_item mood_work" >
+                        Work
+                    </div>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
-                <OverlayTrigger placement="auto" overlay={tooltip}>
                     <div className="mood_item mood_relax">
                         Relax
                     </div>
-                    </OverlayTrigger>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
-                <OverlayTrigger placement="auto" overlay={tooltip}>
                     <div className="mood_item mood_happy">
                         Happy
                     </div>
-                    </OverlayTrigger>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
-                <OverlayTrigger placement="auto" overlay={tooltip}>
                     <div className="mood_item mood_sad">
                         Sad
                     </div>
-                    </OverlayTrigger>
                 </Col>
             </Row>
-        </div>
+        </div >
 
     )
 }
