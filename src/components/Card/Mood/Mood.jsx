9dfd9cbeed2e8e06689ae2b297d218fb7eab423d
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-01-16 21:16:17 +0330

    fix feature Coming soon

--- src/components/Card/Mood/Mood.jsx
+++ src/components/Card/Mood/Mood.jsx
@@ -1,38 +1,37 @@
 import React from 'react'
-import {  Col, OverlayTrigger, Row, Tooltip } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 export default function Mood() {
-    const tooltip = (
-        <Tooltip id="tooltip">
-            <strong>Coming soon!</strong>
-        </Tooltip>
-    );
+
     return (
         <div className='mt-2'>
 
-            <OverlayTrigger placement="top-start" overlay={tooltip}>
-                <h5>
-                    What is your Mood?
-                </h5></OverlayTrigger>
+            <h5>
+                What is your Mood?
+            </h5>
             <Row className='mood'>
                 <Col xxl={3} xl={3} lg={3} md={3}>
                     <div className="mood_item mood_work" >
                         Work
+                        <strong className='d-block'>Coming soon!</strong>
                     </div>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
                     <div className="mood_item mood_relax">
                         Relax
+                        <strong className='d-block'>Coming soon!</strong>
                     </div>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
                     <div className="mood_item mood_happy">
                         Happy
+                        <strong className='d-block'>Coming soon!</strong>
                     </div>
                 </Col>
                 <Col xxl={3} xl={3} lg={3} md={3}>
                     <div className="mood_item mood_sad">
                         Sad
+                        <strong className='d-block'>Coming soon!</strong>
                     </div>
                 </Col>
             </Row>
